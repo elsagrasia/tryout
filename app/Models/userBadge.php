98f7115f87e7cc1bd7e8d636ea479b7d2f0c3e3f
talentkinayom <talenta.kinayom@ti.ukdw.ac.id>
2025-12-13 21:04:54 +0700

    typo

--- app/Models/userBadge.php
+++ app/Models/userBadge.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class userBadge extends Model
+class UserBadge extends Model
 {
     use HasFactory;
     protected $guarded = [];
